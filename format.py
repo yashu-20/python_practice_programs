65f0bd2a57359e715f31fc0b534138d8d3ea1c8f
yashu-20 <69629481+yashu-20@users.noreply.github.com>
2021-05-21 15:37:46 +0530

    create format.py

---
+++ format.py
@@ -0,0 +1,17 @@
+#implement python code to format the character,hexa,and octal
+ch='A'
+print('The character is %c'%(ch))
+a=20
+print('Hexa Number is %X'%(a))
+b=10
+print('Octal Number is %o'%(b))
+name='CSE-A'
+print('Hello %s !How are you'%(name))
+age=20
+print('Hello %s !Your age is %d'%(name,age))
+#output:
+The character is A
+Hexa Number is 14
+Octal Number is 12
+Hello CSE-A !How are you
+Hello CSE-A !Your age is 20
